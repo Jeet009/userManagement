9cb73bb84efaed608ca3982f72524cf275a45799
Jeet Mukherjee <jeetmukherjee009@gmail.com>
2020-07-16 01:33:18 +0530

    final commit

--- src/components/IndexComponent.js
+++ src/components/IndexComponent.js
@@ -7,7 +7,6 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import { Container } from "@material-ui/core";
 import AddFormComponent from "./AddFormComponent";
 
 function TabPanel(props) {
